f83625c820647d47fcf1ee3ee179995272c6985a
Ohanz <chiaguguadaniel@gmail.com>
2024-12-06 14:27:10 +0100

    added UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,40 @@ class TipCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    void myPopBillTip(String val){
+      // Generate dialog
+      String currentVal = (val == "Bill") ? "Bill": "Tip";
+      AlertDialog dialog =  AlertDialog(
+        content: const Text("Hye! Empty \$currentVal. \n"
+            "Add Your \$currentVal to Proceed.",
+            style: TextStyle(
+              fontWeight: FontWeight.w600, // light// italic
+              fontSize: 30.0,))
+        ,actions: [
+        TextButton(child: const Text('Dismiss'),
+          onPressed: () { Navigator.pop(context); // Close the dialog
+          },),],);
+
+      // Show dialog
+      showDialog(context: context, builder: (BuildContext context) => dialog);
+    }
+
+    void myPopVoid(){
+      // Generate dialog
+      AlertDialog dialog =  AlertDialog(
+        content: const Text("Hye! Empty Bill. \n"
+            "Add Your Bill to Proceed.",
+            style: TextStyle(
+              fontWeight: FontWeight.w600, // light// italic
+              fontSize: 30.0,))
+        ,actions: [
+        TextButton(child: const Text('Dismiss'),
+          onPressed: () { Navigator.pop(context); // Close the dialog
+          },),],);
+
+      // Show dialog
+      showDialog(context: context, builder: (BuildContext context) => dialog);
+    }
     void myPop(){
       // Generate dialog
       AlertDialog dialog =  AlertDialog(
@@ -29,6 +63,13 @@ class TipCalculator extends StatelessWidget {
       showDialog(context: context, builder: (BuildContext context) => dialog);
     }
 
+    // Hierarchy of Widgets
+    Container h1 = Container(margin: const EdgeInsets.only(top: 25, bottom: 20),
+      height: 35,
+      child: const Text("Fill your Input to Calculate your Final Bill",
+        style: TextStyle(color: Colors.green,
+            fontSize: 30),),
+    );
 
     // Create first input field
     TextField billAmountField = TextField(
@@ -71,40 +112,56 @@ class TipCalculator extends StatelessWidget {
         ),
         child: const Text("Calculate", style: TextStyle(fontSize: 25, color: Colors.green),),
         onPressed: () {
-          // Calculate tip and total
-          double calculatedTip = billAmount * tipPercentage / 100.0;
-          double total = billAmount + calculatedTip;
+          if( tipPercentage == 0){
+            // pop Add value
+            myPopBillTip("tip");
+          }
+          else if(billAmount == 0){
+            // pop Add value
+            myPopBillTip("Bill");
+          }
+          else if(billAmount == 0 || tipPercentage == 0){
+            // pop Add value
+            myPopVoid();
+          }
+          else{
+            // run Ops
+            // Calculate tip and total
+            double calculatedTip = billAmount * tipPercentage / 100.0;
+            double total = billAmount + calculatedTip;
 
-          myPop();
-          Future.delayed(Duration(seconds: 10), () {
-            print('Delayed code executed');
-            // Generate dialog
-            AlertDialog dialog = AlertDialog(
-                content: Text("Hye Again, Here You Go..\n"
-                    "Tip: \$$calculatedTip \n"
-                    "The Total Bal: \$$total",
-                  style: TextStyle(
-                  fontWeight: FontWeight.w300, // light
-                  fontStyle: FontStyle.italic, // italic
-                    fontSize: 50.0,
-                ),));
+            myPop();
+            Future.delayed(Duration(seconds: 7), () {
+              print('Delayed code executed');
+              // Generate dialog
+              AlertDialog dialog = AlertDialog(
+                  content: Text("Hye Again, Here You Go..\n"
+                      "Tip: \$$calculatedTip \n"
+                      "The Total Bal: \$$total",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300, // light
+                      fontStyle: FontStyle.italic, // italic
+                      fontSize: 50.0,
+                    ),));
 
-            // Show dialog
-            showDialog(context: context, builder: (BuildContext context) => dialog);
-          });
-          Future.delayed(Duration(seconds: 15), (){
-            final snackBar = SnackBar(
-              content: const Text('Yay! That was fantastic!'),
-              action: SnackBarAction(
-                label: 'Dismiss',
-                onPressed: () {
-                  // Some code to undo the change.
-                  print("Dismiss the snack");
-                },
-              ),
-            );
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          },);
+              // Show dialog
+              showDialog(context: context, builder: (BuildContext context) => dialog);
+            });
+            Future.delayed(Duration(seconds: 15), (){
+              final snackBar = SnackBar(
+                content: const Text('Yay! That was fantastic!'),
+                action: SnackBarAction(
+                  label: 'Dismiss',
+                  onPressed: () {
+                    // Some code to undo the change.
+                    print("Dismiss the snack");
+                  },
+                ),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },);
+          }
+
 
         });
     Container cY = Container(margin: const EdgeInsets.only(top: 75, bottom: 20),
@@ -146,8 +203,11 @@ class TipCalculator extends StatelessWidget {
     Container container = Container(
       color: Colors.redAccent,
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-            children: [billAmountField, tipPercentageField, cX, calculateButton,cY, fActionButton]));
+        child:
+            SingleChildScrollView(child: Column(
+                children: [h1, billAmountField, tipPercentageField, cX, calculateButton,cY, fActionButton])
+            ));
+
 
     AppBar appBar = AppBar(title: Text("The GO-TIP"));
 
